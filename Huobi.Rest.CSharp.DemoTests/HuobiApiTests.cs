27703282ca4ea95d857ed79aebe3b2a51d20bf02
hyc <519544044@qq.com>
2018-02-05 15:15:39 +0800

    添加SendRequest

--- Huobi.Rest.CSharp.DemoTests/HuobiApiTests.cs
+++ Huobi.Rest.CSharp.DemoTests/HuobiApiTests.cs
@@ -5,7 +5,7 @@ namespace Huobi.Rest.CSharp.Demo.Tests
     [TestClass()]
     public class HuobiApiTests
     {
-        HuobiApi api = new HuobiApi("","");
+        HuobiApi api = new HuobiApi("ed80fc08-ae45ccce-b538d02a-3d8d2", "01dc4111-73ea09ec-9ee9a534-9e412");
         [TestMethod()]
         public void GetAllAccountTest()
         {
